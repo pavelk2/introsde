c7f5f0f4af8633b52e686935ba9426709a408abe
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2014-09-18 13:41:33 +0200

    my work for lab01

--- lab01/Examples/src/HealthProfileReader.java
+++ lab01/Examples/src/HealthProfileReader.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Date;
 
 import pojos.HealthProfile;
 import pojos.Person;
@@ -7,7 +8,7 @@ import pojos.Person;
 
 public class HealthProfileReader {
 	
-	public static Map<String,Person> database = new HashMap<String,Person>();
+	public static Map<Long,Person> database = new HashMap<Long,Person>();
 	
 	static
     {
@@ -16,9 +17,13 @@ public class HealthProfileReader {
 		HealthProfile hp = new HealthProfile(68.0,1.72);
 		Person john = new Person("John","Doe",hp);
 		
-		database.put(pallino.getFirstname()+" "+pallino.getLastname(), pallino);
-		database.put(pallo.getFirstname()+" "+pallo.getLastname(), pallo);
-		database.put(john.getFirstname()+" "+john.getLastname(), john);
+		HealthProfile pavel_hp = new HealthProfile(76.0,1.86);
+		Person pavel = new Person("Pavel","Kucherbaev",pavel_hp);
+		
+		database.put(pallino.getPersonId(), pallino);
+		database.put(pallo.getPersonId(), pallo);
+		database.put(john.getPersonId(), john);
+		database.put(Long.valueOf(1), pavel);
     }
 	/**
 	 * The health profile reader gets information from the command line about
@@ -27,22 +32,56 @@ public class HealthProfileReader {
 	 * 
 	 * @param args
 	 */
+	public static void createPerson(Long personId, String firstname, String lastname, String birthdate){
+		Person p = new Person(personId,firstname,lastname, birthdate);
+		database.put(p.getPersonId(),p);
+	}
+	public static void displayHealthProfile(Long personId){
+		Person p= database.get(personId);
+		if (p!=null) { 
+			System.out.println(p.getFirstname()+" "+p.getLastname()+"'s health profile is: "+p.gethProfile().toString());
+		}else{
+			System.out.println(String.valueOf(personId)+" is not in the database 1");
+		}
+	}
+	public static void updateHealthProfile(Long personId, Double height, Double weight){
+		Person p= database.get(personId);
+		if (p!=null) { 
+			HealthProfile hprofile = p.gethProfile();
+			hprofile.setHeight(height);
+			hprofile.setWeight(weight);
+			
+			System.out.println(p.getFirstname()+" "+p.getLastname()+"'s health profile is: "+p.gethProfile().toString());
+		}else{
+			System.out.println(String.valueOf(personId)+" is not in the database");
+		}
+	}
 	public static void main(String[] args) {
-		//initializeDatabase();
 		int argCount = args.length;
 		if (argCount == 0) {
-			System.out.println("I cannot create people out of thing air. Give me at least a name and lastname.");
+			System.out
+					.println("I cannot create people out of thing air. Give me at least a name and lastname.");
 		} else if (argCount < 2) {
-			System.out.println("Are you sure you gave me a first and lastname?");
-		} else if (argCount == 2) {
-			String fname = args[0];
-			String lname = args[1];
-			// read the person from the DB
-			Person p= database.get(fname+" "+lname);
-			if (p!=null) { 
-				System.out.println(fname+" "+lname+"'s health profile is: "+p.gethProfile().toString());
+			System.out
+					.println("Are you sure you gave me ALL the information I need?");
+		} else {
+			String method = args[0];
+			if (method.equals("createNewPerson")) {
+				Long personId = Long.parseLong(args[1]);
+				String firstname = args[2];
+				String lastname = args[3];
+				String birthdate = args[4];
+				createPerson(personId, firstname, lastname, birthdate);
+			} else if (method.equals("displayHealthProfile")) {
+				Long personId = Long.parseLong(args[1]);
+				displayHealthProfile(personId);				
+			}  else if (method.equals("updateHealthProfile")) {
+				Long personId = Long.parseLong(args[1]);
+				Double height = Double.parseDouble(args[2]);
+				Double weight = Double.parseDouble(args[3]);
+				updateHealthProfile(personId, height, weight);
 			} else {
-				System.out.println(fname+" "+lname+" is not in the database");
+				System.out.println("The system did not find the method '"+method+"'");
 			}
 		}
 		// add the case where there are 3 parameters, the third being a string that matches "weight", "height" or "bmi"
--- lab01/Examples/src/pojos/Person.java
+++ lab01/Examples/src/pojos/Person.java
@@ -2,36 +2,76 @@
 // Classes in the same pakage already see each other. 
 // If a class is in another package, in other to see it, you need to import it
 package pojos;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
 
 // This is a typical Java Class. 
 public class Person {
 
-	// As with any other object oriente language, classes have attributes (i.e. the properties of the class). 
+	// As with any other object oriented language, classes have attributes (i.e. the properties of the class). 
 	// Each attribute is in turn of another class
 	private String firstname;	// this is an attribute of the class String, and it is 'private'
 								// private attributes are only accesible inside the object
 	private String lastname;	// this is an attribute of the class String
 	private HealthProfile hProfile;	// this is an attribute of the class HealthProfile 
-	
+	private String birthday; // this is birthday
+	private Long personId;
 	// constructors in java are used to create an object of the class 
 	// (a java program basically plays with objects of different classes)
 	// this constructor creates a Person object with a particular firstname, lastname and health profile
-	public Person(String fname, String lname, HealthProfile hp) {
+	public Person(String fname, String lname, HealthProfile hp, String bday) {
 		this.setFirstname(fname);
 		this.setLastname(lname);
+		this.setBirthday(bday);
+		
+		this.hProfile=hp;
+	}
+	public Person(String fname, String lname, String bday) {
+		this.setPersonId(randInt(1,9999));
+		this.setFirstname(fname);
+		this.setLastname(lname);
+		this.setBirthday(bday);
+	}
+	public Person(Long personId, String fname, String lname, String bday) {
+		this.setPersonId(personId);
+		this.setFirstname(fname);
+		this.setLastname(lname);
+		this.setBirthday(bday);
+	}
+	public Person(String fname, String lname, HealthProfile hp) {
+		this.setPersonId(randInt(1,9999));
+		this.setFirstname(fname);
+		this.setLastname(lname);
+		
 		this.hProfile=hp;
 	}
 	public Person(String fname, String lname) {
+		this.setPersonId(randInt(1,9999));
 		this.setFirstname(fname);
 		this.setLastname(lname);
 		this.hProfile=new HealthProfile();
 	}
 	public Person() {
+		this.setPersonId(randInt(1,9999));
 		this.firstname="Pinco";
 		this.lastname="Pallino";
 		this.hProfile=new HealthProfile();
 	}
+	
+	public static Long randInt(int min, int max) {
 
+	    // NOTE: Usually this should be a field rather than a method
+	    // variable so that it is not re-seeded every call.
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    return Long.valueOf(rand.nextInt((max - min) + 1) + min);
+	}
 	// classes have methods, which are basically pieces of programs that can be executed on objects of the class
 	// this dummy class, has only 'accesor' methods (i.e. methods to access its properties, which are all private)
 	public String getFirstname() {
@@ -43,8 +83,30 @@ public class Person {
 	public String getLastname() {
 		return lastname;
 	}
+	public Long getPersonId() {
+		return personId;
+	}
+	public String getBirthday() {
+		//String formattedDate = new SimpleDateFormat("dd/MM/yyyy, Ka").format(date));
+		return birthday;
+		
+	}
+	public void setPersonId(Long id) {
+		this.personId = id;
+	}
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String randomBirthday(){
+		/*long offset = Timestamp.valueOf("1950-01-01 00:00:00").getTime();
+		long end = Timestamp.valueOf("2013-01-01 00:00:00").getTime();
+		long diff = end - offset + 1;
+		Timestamp rand = new Timestamp(offset + (long)(Math.random() * diff));
+		*/
+		return "123";
 	}
 	public HealthProfile gethProfile() {
 		return hProfile;
